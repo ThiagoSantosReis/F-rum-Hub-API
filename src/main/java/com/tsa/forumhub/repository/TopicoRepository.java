327643d3bc8f4dfe6ace4cd315c73e913748d495
thiago.san <thisantosareis@gmail.com>
2025-08-12 19:04:04 -0300

    set up data repository for managing topico records

---
+++ src/main/java/com/tsa/forumhub/repository/TopicoRepository.java
@@ -0,0 +1,7 @@
+package com.tsa.forumhub.repository;
+
+import com.tsa.forumhub.model.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+}
