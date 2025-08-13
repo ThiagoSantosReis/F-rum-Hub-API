c3099577e260cd1316e07e54548856da39437fea
thiago.san <thisantosareis@gmail.com>
2025-08-13 18:14:58 -0300

    create database migration for usuarios table

---
+++ src/main/java/com/tsa/forumhub/model/Usuario.java
@@ -0,0 +1,68 @@
+package com.tsa.forumhub.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    private String senha;
+
+    private String role = "ROLE_USER";
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/resources/db/migration/V3__insert_into_usuarios_admin.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuarios (login, senha, role)
+VALUES (
+  'admin',
+  '$2a$10$9e1k9kO4u3xOq0Y8CkMRMeZ8m3o6b8zR2N2q6M3Q3lJqW5K5m7O3a',
+  'ROLE_ADMIN'
+);
