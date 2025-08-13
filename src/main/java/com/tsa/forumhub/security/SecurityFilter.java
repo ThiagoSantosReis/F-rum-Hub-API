51410e53e816be41605a55f2c0b676d0c9d31824
thiago.san <thisantosareis@gmail.com>
2025-08-13 18:52:35 -0300

    validate jwt token and set authentication context with a security filter

--- src/main/java/com/tsa/forumhub/security/SecurityFilter.java
+++ src/main/java/com/tsa/forumhub/security/SecurityFilter.java
@@ -1,4 +1,48 @@
 package com.tsa.forumhub.security;
 
-public class SecurityFilter {
+import com.tsa.forumhub.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private  TokenService tokenService;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            token = authHeader.substring(7);
+        }
+
+        if(token != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            String username = tokenService.getSubject(token);
+            if(username!=null){
+                var usuario = usuarioRepository.findByLogin(username).orElse(null);
+                if(usuario!=null){
+                    var authToken = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
 }
--- src/main/java/com/tsa/forumhub/security/TokenService.java
+++ src/main/java/com/tsa/forumhub/security/TokenService.java
@@ -4,11 +4,13 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+@Service
 public class TokenService {
     @Value("${api.security.jwt.secret}")
     private String secret;
